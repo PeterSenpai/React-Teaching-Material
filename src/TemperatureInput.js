d649cd0e1a18997eb75ae4a90d48786d4a9d1e19
Peter T <petertao9t@gmail.com>
2020-11-28 13:02:49 -0500

    some comments

--- src/TemperatureInput.js
+++ src/TemperatureInput.js
@@ -6,10 +6,10 @@ const scaleNames = {
 };
 
 export default class TemperatureInput extends Component {
-  //   constructor(props) {
-  //     super(props);
-  //     this.handleChange = this.handleChange.bind(this);
-  //   }
+  constructor(props) {
+    super(props);
+    // this.handleChange = this.handleChange.bind(this);
+  }
 
   handleChange = (e) => {
     this.props.onTemperatureChange(e.target.value);
